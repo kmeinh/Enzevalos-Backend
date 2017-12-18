83ec71f2b8d8c43ccfa72a8092b4e4691bea2f37
kondeichmann <k.deichmann@mac.com>
2017-12-18 12:24:42 +0100

    map mongo to mongo url

--- Server/config/config.js
+++ Server/config/config.js
@@ -2,8 +2,8 @@ let env = process.env.NODE_ENV || 'development'
 
 if (env === 'development') {
     process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://159.89.31.10:27017/Server-bsc'
+    process.env.MONGODB_URI = 'mongodb://mongo:27017/Server-bsc'
 } else if (env === 'test') {
     process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://159.89.31.10:27017/Server-bsc-test'
+    process.env.MONGODB_URI = 'mongodb://mongo:27017/Server-bsc-test'
 }
